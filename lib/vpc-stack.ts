499105c38f8cb07bb42410692c38a1b5009f668e
Elias Delgado <evdelgado99@gmail.com>
2025-06-05 08:34:58 -0400

    feat: add VPC stack

---
+++ lib/vpc-stack.ts
@@ -0,0 +1,41 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { Construct } from 'constructs';
+
+// VPC Stack for ReelQuotes Infrastructure
+// This stack creates a VPC with public and private subnets across two AZs
+export class VpcStack extends cdk.Stack {
+  public readonly vpc: ec2.Vpc;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.vpc = new ec2.Vpc(this, 'ReelQuotesVPC', {
+      maxAzs: 2,
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+      ],
+    });
+
+    // Output the VPC ID and private subnet IDs
+    new cdk.CfnOutput(this, 'VpcId', {
+      value: this.vpc.vpcId,
+      description: 'The ID of the VPC',
+    });
+
+    new cdk.CfnOutput(this, 'PrivateSubnetIds', {
+      value: this.vpc.privateSubnets.map(subnet => subnet.subnetId).join(','),
+      description: 'The IDs of the private subnets',
+    });
+  }
+} 
